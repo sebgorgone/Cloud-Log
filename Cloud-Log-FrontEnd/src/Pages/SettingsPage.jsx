2b72d2eeba3f2677342ebbe173d237026b596c74
SGORGONE <sebgorgone@gmail.com>
2025-06-17 16:04:42 -0400

    user settings field built out with css included

--- Cloud-Log-FrontEnd/src/Pages/SettingsPage.jsx
+++ Cloud-Log-FrontEnd/src/Pages/SettingsPage.jsx
@@ -13,9 +13,62 @@ function SettingsPage(props) {
 
    const [userCred, setUserCred] = useState(null);
 
+   const [usernameField, setUsernameField] = useState(false);
+
+   const [newUsername, setNewUsername] = useState();
+
+   const [emailField, setEmailField] = useState(false);
+
+   const [newEmail, setNewEmail] = useState();
+
+   const [passwordField, setPasswordField] = useState(false);
+
+   //handlers
+
+   function handleCUCLick (e) {
+      e.preventDefault();
+      setEmailField(false);
+      setNewEmail(null)
+      setUsernameField(true);
+   }
+
+   function handleCUCancel (e) {
+      e.preventDefault();
+      setNewUsername(null)
+      setUsernameField(false)
+   }
+
+   function handleNewUserChange (e) {
+      setNewUsername(e.target.value);
+      console.log(newUsername)
+   }
+
+   function handleCEClick (e) {
+      e.preventDefault();
+      setUsernameField(false)
+      setNewUsername(null)
+      setEmailField(true);
+
+   }
+
+   function handleCECancel (e) {
+      e.preventDefault();
+      setNewEmail(null)
+      setEmailField(false)
+   }
+
+   function handleNewEmailChange (e) {
+      setNewEmail(e.target.value);
+      console.log(newEmail);
+   }
+
+   function handleCPClick (e) {}
+
+   //time stamp jsx
+
    function createdAt () {
 
-      if (!userCred) return 'loading...';
+      if (!userCred) return <p>loading...</p>;
 
       const raw = userCred[0].created_at;
       
@@ -43,7 +96,7 @@ function SettingsPage(props) {
       const ampm   = hour24 >= 12 ? 'pm' : 'am';
       const hour12 = hour24 % 12 || 12; // turns 0→12, 13→1, 12→12
 
-      return `Created ${diffDays} Day${diffDays !== 1 ? 's' : ''} ago '${monthName} ${createdDate.getDate()}, ${createdDate.getFullYear()}' at ${hour12}:${minute} ${ampm}`;
+      return <p>Created {diffDays} Day{diffDays !== 1 ? 's' : ''} ago <br />'{monthName} {createdDate.getDate()}, {createdDate.getFullYear()}' at {hour12}:{minute} {ampm}</p>;
    }
   
    console.log('in the Settings page', userCred);
@@ -56,21 +109,94 @@ function SettingsPage(props) {
       paddingLeft: "1em",
       paddingBottom: "1em",
       background: pallette[2],
-      textAlignLast: "center",
       display: "flex",
       flexWrap: "wrap",
-      justifyContent: "space-evenly"
+      justifyContent: "space-around"
    }
 
    const headerStyle = {
       width: "100%",
       fontFamily: "L1",
+      color: pallette[0],
+      textAlign: "center"
    }
 
    const settingsStyle = {
       display: "flex",
       flexFlow: "column",
+      width: "40%"
+   }
 
+   const timeStamp = {
+      fontSize: ".6em",
+      fontFamily: "L1",
+      color: pallette[0]
+   }
+
+   const changePasswordButton = {
+      border: "none",
+      fontFamily: "L1",
+      width: "70%",
+      borderRadius: "1em",
+      paddingBottom: ".2em",
+      background: pallette[1],
+      color: pallette[4],
+   }
+
+   const changeButton = {
+      border: "none",
+      marginBottom: "1em",
+      fontFamily: "L1",
+      width: "50%",
+      borderRadius: "1em",
+      paddingBottom: ".2em",
+      background: pallette[3],
+      color: pallette[0],
+   }
+
+   const inputStyle = {
+      width: "1000vw",
+      fontFamily: "L1",
+      fontSize: "1.em",
+      padding: ".2em",
+      margin: "0",
+      marginBottom: ".5em",
+      background: pallette[3],
+      color: pallette[0]
+   }
+
+   const textStyle = {
+      fontFamily: "L1",
+      margin: "0",
+      marginTop: "0",
+      marginBottom: ".5em",
+      background: pallette[4],
+      borderRadius: "1em",
+      color: pallette[0],
+      width: "100%",
+      padding: ".5em",
+      textAlign: "center",
+   }
+
+   const nestedButtonOk = {
+      border: "none",
+      fontSize: ".6em",
+      fontFamily: "L1",
+      borderRadius: "1em",
+      paddingBottom: ".4em",
+      background: pallette[1],
+      color: pallette[4],
+   }
+
+   const nestedButtonCancel = {
+      border: "none",
+      fontSize: ".6em",
+      fontFamily: "L1",
+      borderRadius: "1em",
+      paddingBottom: ".4em",
+      background: pallette[4],
+      color: pallette[1],
+      marginLeft: ".6em"
    }
 
    //api
@@ -112,19 +238,57 @@ function SettingsPage(props) {
 
             <h1 style={headerStyle}>Settings</h1>
 
+            {passwordField ? 
+            
+            <div>
+               <p>pasword prompt</p>
+            </div> 
+            
+            : 
+            
             <div style={settingsStyle}>
 
-               <input 
-                  placeholder={user.name} 
-               />
 
-               <input 
-                  placeholder={userCred ? userCred[0].email : 'loading...'} 
-               />
+               <p style={textStyle}>User: {user ? user.name: 'loading...'}</p>
 
-               <button>change password</button>
-               <p>{createdAt()}</p>
-            </div>
+               {!usernameField && <button style={changeButton} onClick={handleCUCLick}>change username</button>}
+
+               {usernameField && <input 
+                  value={newUsername}
+                  onChange={handleNewUserChange}
+                  style={inputStyle}
+                  placeholder='change username' 
+               />}
+
+               {usernameField && <div style={{marginBottom: ".75em"}}>
+                  <button style={nestedButtonOk}>change username</button>
+                  <button style={nestedButtonCancel} onClick={handleCUCancel}>cancel</button>
+               </div>}
+
+               <p style={textStyle}>Email: {userCred ? userCred[0].email : 'loading...'}</p>
+
+               {!emailField && <button style={changeButton} onClick={handleCEClick}>change email</button>}
+
+               {emailField && <input 
+                  value={newEmail}
+                  onChange={handleNewEmailChange}
+                  style={inputStyle}
+                  placeholder="change email" 
+               />}
+
+               {emailField && <div style={{marginBottom: ".75em"}}>
+                  <button style={nestedButtonOk}>change email</button>
+                  <button style={nestedButtonCancel} onClick={handleCECancel}>cancel</button>
+               </div>}
+
+
+
+               <button style={changePasswordButton}>change password</button>
+
+               <p style={timeStamp}>{createdAt()}</p>
+
+
+            </div>}
 
             
          </div>
