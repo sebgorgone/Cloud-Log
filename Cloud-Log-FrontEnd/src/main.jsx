7488ba9612105690fbd21432739cd664c80d51a2
SGORGONE <sebgorgone@gmail.com>
2025-06-26 19:54:17 -0400

    same bug fix

--- Cloud-Log-FrontEnd/src/main.jsx
+++ Cloud-Log-FrontEnd/src/main.jsx
@@ -4,7 +4,7 @@ import LoginScreen from './Pages/loginScreen.jsx'
 import {BrowserRouter, Routes, Route} from  'react-router-dom';
 import PrivateRoute from './routes/PrivateRoute.jsx';
 import { AuthProvider } from './contexts/authContext.jsx';
-import HomePage from './Pages/HomePage.jsx'
+import HomePage from './Pages/homePage.jsx'
 import RegisterPage from './Pages/RegisterPage.jsx';
 import NoPage from './Pages/NoPage.jsx';
 import LoadPage from './Pages/LoadPage.jsx';
