1cd987de231b532771b0e193a20ee10eb19d1b3d
SGORGONE <sebgorgone@gmail.com>
2025-07-12 07:18:04 -0400

    cloudlog v0.3

--- Server/server.js
+++ Server/server.js
@@ -73,16 +73,6 @@ const permittedTables = [
  app.use(express.json({ limit: 
   '10mb' }));
 
-  app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // or specific domains
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
-
  app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use(helmet());
